38de11eb1260882869d197827dc12bc11e7a8a2b
JeffDanielle <bayotjeff@gmail.com>
2024-04-29 14:01:11 +0800

    84. More on Type Guards

--- adv-types-02-intersection-types/adv-types-02-intersection-types/src/app.js
+++ adv-types-02-intersection-types/adv-types-02-intersection-types/src/app.js
@@ -3,3 +3,50 @@ var e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    // type guard with 'in' from typescript
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving...");
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Driving a truck...");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("Loading cargo..." + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    // type guard from vanilla javascript
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
