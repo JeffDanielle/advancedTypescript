7fa50403cbf669586b8a3dfee4e31025d5d95195
JeffDanielle <bayotjeff@gmail.com>
2024-04-29 14:09:14 +0800

    85. Discriminated Unions

--- adv-types-02-intersection-types/adv-types-02-intersection-types/src/app.ts
+++ adv-types-02-intersection-types/adv-types-02-intersection-types/src/app.ts
@@ -75,3 +75,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+    // Type assignment
+    type: "bird";
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: "horse";
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+
+    console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({type: "bird", flyingSpeed: 10});
