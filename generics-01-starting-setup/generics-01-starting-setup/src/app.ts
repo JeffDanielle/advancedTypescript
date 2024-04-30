da449d37456296bb5eb201e33f9fbd35336d57d5
JeffDanielle <bayotjeff@gmail.com>
2024-04-30 14:31:11 +0800

    96. Working with Constraints

--- generics-01-starting-setup/generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/generics-01-starting-setup/src/app.ts
@@ -16,7 +16,7 @@
 // });
 
 // different way of doing generics, concrete Types
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
