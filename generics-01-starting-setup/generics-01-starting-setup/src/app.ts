7daa94935ce77e0724e4cf90b5517cf14dfc73bd
JeffDanielle <bayotjeff@gmail.com>
2024-04-30 14:16:05 +0800

    94. Built-in Generics & What are Generics?

---
+++ generics-01-starting-setup/generics-01-starting-setup/src/app.ts
@@ -0,0 +1,16 @@
+// Generics, a type connected to another type: example below
+// Array of string
+// const names: Array<string> = [];
+
+// // names[0].split(" ");
+
+// // Promise expected to resolve with a number
+// const promise = new Promise<number>((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10);
+//     }, 2000);
+// });
+
+// promise.then((data) => {
+//     // data.split(" ");
+// });
