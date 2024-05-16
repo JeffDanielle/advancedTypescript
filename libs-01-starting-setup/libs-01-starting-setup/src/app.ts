0e8292ea43081c50152a9eb2c9746df2b59b1b16
JeffDanielle <bayotjeff@gmail.com>
2024-05-16 14:07:26 +0800

    165. TypeScript-embracing:class-validator

--- libs-01-starting-setup/libs-01-starting-setup/src/app.ts
+++ libs-01-starting-setup/libs-01-starting-setup/src/app.ts
@@ -2,6 +2,7 @@
 import "reflect-metadata";
 import {plainToClass} from "class-transformer";
 import {Product} from "./product.model";
+import {validate} from "class-validator";
 
 const products = [
     {title: "A Carpet", price: 29.99},
@@ -14,11 +15,22 @@ const products = [
 //     return new Product(prod.title, prod.price);
 // });
 
+// import from class transformer which shorten code to reflect metadata
 const loadedProducts = plainToClass(Product, products);
 
 for (const prod of loadedProducts) {
     console.log(prod.getInformation());
 }
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+    if (errors.length > 0) {
+        console.log("VALIDATION ERRORS!");
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
 
 // console.log(_.shuffle([1, 2, 3]));
 
